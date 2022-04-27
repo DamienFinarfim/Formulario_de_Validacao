a58ef1d92cd3b7d2d5d4fd1cc1dcb4cf995911c7
Alexandre Silva <alexandreasilva@protonmail.com>
2022-04-26 21:40:41 -0300

    Formulário para preenchimento

---
+++ script.js
@@ -0,0 +1,46 @@
+document.querySelector('form').addEventListener("click", function (e) {
+    const camposEmail = document.querySelector('#email');
+    const capturaEmail = camposEmail.value;
+    function validaEmail() {
+        const ev = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
+        return ev.test(capturaEmail)
+    }
+    let capturaFuncaoValidaEmail = validaEmail(capturaEmail);
+    console.log(capturaFuncaoValidaEmail)
+    if (capturaFuncaoValidaEmail === true) {
+        document.querySelector('.erro de Email').innerHTML = "";
+    }
+    console.log(capturaFuncaoValidaEmail)
+});
+document.querySelector('form').addEventListener("click", function (e) {
+    const chaveCEP = this.querySelector('#cep');
+    const valorCEP = chaveCEP.value;
+    function validaCEP() {
+        const regexCEP = /^[0-9]{5}-[0-9]{3}$/;
+        return regexCEP.test(valorCEP)
+    }
+    let capturaFuncaoCEP = validaCEP(valorCEP)
+    console.log(capturaFuncaoCEP)
+    if (capturaFuncaoCEP === true) {
+        document.querySelector('.erro de CEP').innerHTML = "";
+    }
+});
+document.querySelector('form').addEventListener("click", function (e) {
+    const ufSelect = document.querySelector("#uf");
+    const valueUF = ufSelect.value;
+    console.log(valueUF)
+    function validar(uf) {
+        var er = /A[CLMP]|BA|CE|ES|M[AGST]|P[ABEIR]|R[JNORS]|S[CEP]|[GT]O/i;
+        return er.test(uf);
+    }
+    let ufSelected = validar(valueUF)
+    console.log(ufSelected)
+    if (ufSelected === true) {
+        document.querySelector('.erro de UF').innerHTML = "";
+    }
+});
+document.querySelector("form")
+    .addEventListener('submit', e => {
+        //console.log("enviar o formulário")
+        e.preventDefault();
+    })
